239e61180b41672e28a7861597a00f1b526fd7a7
harmeet <harmeetsingh7276@gmail.com>
2022-07-22 01:14:55 +0530

    If we wish to remove the @requestbody annotation then on the top of the controller you can add @restcontroller also instead of RequestMapping we can use getmapping and postmapping

--- SpringBootJPAMVC/src/main/java/com/example/demo/controller/AlienController.java
+++ SpringBootJPAMVC/src/main/java/com/example/demo/controller/AlienController.java
@@ -5,16 +5,19 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
 import com.example.demo.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 	@Autowired
 	AlienRepo repo;
@@ -38,15 +41,18 @@ public class AlienController {
 		return mv;
 	}
 	
-	@RequestMapping(path="/aliens")
-	@ResponseBody
+	@GetMapping(path="/aliens")
 	public List<Alien> getAliens() {
 		return repo.findAll();
 	}
 	
 	@RequestMapping("/alien/{aid}")
-	@ResponseBody
 	public Optional<Alien> getAlienAPI(@PathVariable("aid") int aid) {
+		return repo.findById(aid);
+	}
+	
+	@PostMapping("/alien")
+	public Optional<Alien> postAlien(@PathVariable("aid") int aid) {
 		return repo.findById(aid);
 	}
 	
